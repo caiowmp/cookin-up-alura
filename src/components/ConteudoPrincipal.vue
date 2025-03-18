909e59951c678711d8a57a83759418f47c7c01e6
Caio Miranda Pereira <71469917+caiowmp@users.noreply.github.com>
2025-03-18 11:21:02 +0000

    começa a utilizar o componente `Tag.vue`

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,5 +1,6 @@
 <script>
 import SelecionarIngredientes from "./SelecionarIngredientes.vue";
+import Tag from "./Tag.vue";
 
 export default {
   data() {
@@ -7,7 +8,7 @@ export default {
       ingredientes: ["Alho", "Mateiga", "Orégano"],
     };
   },
-  components: { SelecionarIngredientes },
+  components: { SelecionarIngredientes, Tag },
 };
 </script>
 
@@ -17,12 +18,8 @@ export default {
       <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li
-          v-for="ingrediente in ingredientes"
-          :key="ingrediente"
-          class="ingrediente"
-        >
-          {{ ingrediente }}
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+          <Tag :texto="ingrediente" />
         </li>
       </ul>
 
@@ -31,7 +28,7 @@ export default {
           src="../assets/imagens/icones/lista-vazia.svg"
           alt="Ícone de pesquisa"
         />
-        Sua lista está vvaiza, selecione ingredientes para iniciar
+        Sua lista está vaiza, selecione ingredientes para iniciar
       </p>
     </section>
 
@@ -64,18 +61,6 @@ export default {
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-  transition: 0.2s;
-  color: var(--creme, #fffaf3);
-  background: var(--coral, #f0633c);
-  font-weight: 700;
 }
 
 .lista-vazia {
